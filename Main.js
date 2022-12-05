ff7d21da8a19d5c12194787c46a665825dad3aab
HugeLetters <ckuka71@gmail.com>
2022-12-05 19:56:14 +0300

    g

--- Main.js
+++ Main.js
@@ -7,10 +7,44 @@ const token = process.env.DISCORD_TOKEN
 const headers = { "Authorization": `Bot ${token}` }
 const url = `https://discord.com/api/v10/applications/${app}/guilds/${guild}/commands`
 
-fetch(url, {
+const json = {
+    "name": "blep",
+    "type": 1,
+    "description": "Send a random adorable animal photo",
+    "options": [
+        {
+            "name": "animal",
+            "description": "The type of animal",
+            "type": 3,
+            "required": true,
+            "choices": [
+                {
+                    "name": "Dog",
+                    "value": "animal_dog"
+                },
+                {
+                    "name": "Cat",
+                    "value": "animal_cat"
+                },
+                {
+                    "name": "Penguin",
+                    "value": "animal_penguin"
+                }
+            ]
+        },
+        {
+            "name": "only_smol",
+            "description": "Whether to show only baby animals",
+            "type": 5,
+            "required": false
+        }
+    ]
+}
+
+const response = fetch(url, {
     method: 'GET',
-    headers: headers,
-    // body: ''
-})
-    .then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)))
+    headers: { ...headers, ...{ "Content-Type": 'application/json' } },
+    // body:JSON.stringify(json),
+}).then(x => x.json());
+
+response.then(response => console.log(JSON.stringify(response)));
