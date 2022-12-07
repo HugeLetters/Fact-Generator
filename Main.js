4aca56f0a8055961ea23adcb361f71da1d9d4df5
HugeLetters <ckuka71@gmail.com>
2022-12-07 16:23:13 +0300

    d

--- Main.js
+++ Main.js
@@ -1,50 +1,32 @@
 import 'dotenv/config';
+import WebSocket from 'ws';
 
 const guild = process.env.GUILD_ID;
 const app = process.env.APP_ID;
 const token = process.env.DISCORD_TOKEN
 
 const headers = { "Authorization": `Bot ${token}` }
-const url = `https://discord.com/api/v10/applications/${app}/guilds/${guild}/commands`
+const protocol = { http: "https", websocket: "wss" }
 
-const json = {
-    "name": "blep",
-    "type": 1,
-    "description": "Send a random adorable animal photo",
-    "options": [
-        {
-            "name": "animal",
-            "description": "The type of animal",
-            "type": 3,
-            "required": true,
-            "choices": [
-                {
-                    "name": "Dog",
-                    "value": "animal_dog"
-                },
-                {
-                    "name": "Cat",
-                    "value": "animal_cat"
-                },
-                {
-                    "name": "Penguin",
-                    "value": "animal_penguin"
-                }
-            ]
-        },
-        {
-            "name": "only_smol",
-            "description": "Whether to show only baby animals",
-            "type": 5,
-            "required": false
-        }
-    ]
-}
+// let url = `${protocol.http}://discord.com/api/v10/applications/${app}/guilds/${guild}`
+let url = `${protocol.http}://discord.com/api/v10/gateway/bot`
 
-const response = fetch(url, {
+const response = await fetch(url, {
     method: 'GET',
-    headers: { ...headers, ...{ "Content-Type": 'application/json' } },
-    // body:JSON.stringify(json),
+    headers: headers
 }).then(x => x.json());
 
-response.then(response => console.log(JSON.stringify(response)));
+url = response.url;
+console.log(url);
+const socket = new WebSocket(url);
+
+socket.onopen = function(e) {
+    console.log("[open] Connection established");
+    socket.send("Hello from HugeLetters!!!");
+  };
+
+socket.onmessage = function(event) {
+    let response=JSON.parse(event.data);
+    // console.log(response);
+    console.log(`[message] Data received from server: ${JSON.stringify(response)}`);
+  };
